0a2ad6d31c29b1a0cc28ccbc32c286a5dde4a415
roeyHonig <roeyhonig94@gmail.com>
2018-05-14 09:52:22 +0300

    1st commit of the day

--- Lec7/ViewController.swift
+++ Lec7/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func showAlertDialog(_ sender: UIButton) {
+        // Discuss View Controller -> present them, pass them some info
+        //*** init UIAlertDialogController
+        let alertCtrl = UIAlertController (title: "Lunch Is Ready", message: "Are You Sure?", preferredStyle: .alert)
+        
+        //*** init an ACTION Button (we need Positive and Negetive Buttons)
+        let yes = UIAlertAction(title: "Yes!", style: UIAlertActionStyle.default) { (btn) in
+            print("Ok was Clicked")
+        }
+        
+        //*** assign the POSITIVE BUtton we've just created to the DialogController
+        alertCtrl.addAction(yes)
+        
+        //*** present it- when a ViewController wants to present another ViewController
+        self.present(alertCtrl, animated: true)
+        // not neccesry to write self
+        // the last argument , completion, actually allows us to write some code to be excuted after completion of the presentation of the viewController
+        
+        
+        
+        
+        
+        //
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
